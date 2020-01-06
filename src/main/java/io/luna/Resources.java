b238c08b83f05bbc42e883b33ba484d2b3a655b7
Dan Searle <dan_searle@outlook.com>
2020-01-06 13:19:59 +0000

    Update Resources.java

--- src/main/java/io/luna/Resources.java
+++ src/main/java/io/luna/Resources.java
@@ -7,8 +7,8 @@ import java.nio.file.Paths;
 public class Resources {
     public static Path find(String resource) {
         try {
-            return Paths.get("./src/main/resources/" + resource);
-            //return Paths.get(ClassLoader.getSystemResource(resource).toURI());
+            return Paths.get("./src/main/resources/" + resource); //Windows workaround
+            //return Paths.get(ClassLoader.getSystemResource(resource).toURI()); //Only works on Mac
         } catch (Exception e) {
             e.printStackTrace();
         }
